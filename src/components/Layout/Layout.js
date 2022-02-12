8f84d79a948dab054f43d5242f00ac3c804a9ea2
nastia260600 <2606nastia00@gmail.com>
2022-02-12 18:48:02 +0200

    sign-in/sign-up

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -29,69 +29,64 @@ const Layout = () => {
 
   return (
     <>
-      {isSuccess ? (
-        <>
-          <AppBar />
-          <Container
-            sx={[
-              theme => ({
-                height: 'calc(100% - 77px)',
-                position: 'relative',
-                zIndex: 20,
-                display: 'flex',
-                justifyContent: 'space-between',
-                [theme.breakpoints.down('desktop')]: {
-                  padding: '0 30px',
-                  flexDirection: 'column',
-                  rowGap: '40px',
-                  justifyContent: 'initial',
-                  height: 'auto',
-                },
-                [theme.breakpoints.down('tablet')]: {
-                  padding: '0 20px',
-                },
-              }),
-            ]}
-          >
-            <Box
-              sx={[
-                theme => ({
-                  paddingTop: '40px',
-                  paddingRight: '30px',
-                  height: '100%',
-                  position: 'fixed',
-                  [theme.breakpoints.down('desktop')]: {
-                    padding: '20px 0 0 0',
-                    width: '100%',
-                    position: 'static',
-                  },
-                }),
-              ]}
-            >
-              <SideMenu />
-            </Box>
-            <Box
-              sx={[
-                theme => ({
-                  paddingLeft: '30px',
-                  paddingTop: '30px',
-                  paddingBottom: '30px',
-                  width: '100%',
-                  marginLeft: '355px',
-                  [theme.breakpoints.down('desktop')]: {
-                    padding: 0,
-                    marginLeft: 0,
-                  },
-                }),
-              ]}
-            >
-              <Outlet />
-            </Box>
-          </Container>
-          <AppBackground />
-        </>
-      ) : (<div></div>)}
-
+      <AppBar />
+      <Container
+        sx={[
+          theme => ({
+            height: 'calc(100% - 77px)',
+            position: 'relative',
+            zIndex: 20,
+            display: 'flex',
+            justifyContent: 'space-between',
+            [theme.breakpoints.down('desktop')]: {
+              padding: '0 30px',
+              flexDirection: 'column',
+              rowGap: '40px',
+              justifyContent: 'initial',
+              height: 'auto',
+            },
+            [theme.breakpoints.down('tablet')]: {
+              padding: '0 20px',
+            },
+          }),
+        ]}
+      >
+        <Box
+          sx={[
+            theme => ({
+              paddingTop: '40px',
+              paddingRight: '30px',
+              height: '100%',
+              position: 'fixed',
+              [theme.breakpoints.down('desktop')]: {
+                padding: '20px 0 0 0',
+                width: '100%',
+                position: 'static',
+              },
+            }),
+          ]}
+        >
+          <SideMenu />
+        </Box>
+        <Box
+          sx={[
+            theme => ({
+              paddingLeft: '30px',
+              paddingTop: '30px',
+              paddingBottom: '30px',
+              width: '100%',
+              marginLeft: '355px',
+              [theme.breakpoints.down('desktop')]: {
+                padding: 0,
+                marginLeft: 0,
+              },
+            }),
+          ]}
+        >
+          <Outlet />
+        </Box>
+      </Container>
+      <AppBackground />
     </>
   );
 };
