e1f7d4fb04317073b6b9af9829bca150e8f8cdd9
Artem Matiushenko <artom.matyushenko@gmail.com>
2022-02-18 13:35:06 +0200

    Created ModalLogout component

--- src/components/ModalLogout/ModalLogout.js
+++ src/components/ModalLogout/ModalLogout.js
@@ -1,10 +1,10 @@
 import { Button } from '@mui/material';
-import { Box } from '@mui/system';
 import { Modal } from 'components/common/Modal';
 import { ROUTES } from 'constants/routes';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router';
 import { reset } from 'redux/globalSlice';
+import * as S from './ModalLogout.style';
 
 const ModalLogout = ({ open, onClose }) => {
   const dispatch = useDispatch();
@@ -18,15 +18,21 @@ const ModalLogout = ({ open, onClose }) => {
 
   return (
     <Modal open={open} onClose={onClose}>
-      <Box sx={{ width: '300px', height: '150px', backgroundColor: '#fff' }}>
-        <h2>Do you really want to log out?</h2>
-        <Button variant="contained" onClick={onClose}>
-          No
-        </Button>
-        <Button variant="contained" onClick={onConfirmHandler}>
-          Yes
-        </Button>
-      </Box>
+      <S.ModalContent>
+        <S.Title>Log out from an account?</S.Title>
+        <S.ButtonsContainer>
+          <Button variant="outlined" color="secondary" onClick={onClose}>
+            No
+          </Button>
+          <Button
+            variant="contained"
+            onClick={onConfirmHandler}
+            disableElevation
+          >
+            Yes
+          </Button>
+        </S.ButtonsContainer>
+      </S.ModalContent>
     </Modal>
   );
 };
